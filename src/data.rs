8813bd119e4177d70ce9e4df09af0aaaa44c3b24
Kasun <kasun.ranasinghe@icloud.com>
2024-07-31 07:48:23 +0530

    Added helper functions to save,load LdapEntry

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,7 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
-use tokio::fs::File;
+use tokio::fs::{self, File};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[derive(Serialize, Deserialize)]
@@ -35,4 +35,27 @@ impl LdapEntry {
         let entry: LdapEntry = bincode::deserialize(&bytes).unwrap();
         Ok(entry)
     }
+}
+
+async fn save_ldap_entry(entry: &LdapEntry) -> std::io::Result<()> {
+    let path = dn_to_path(&entry.dn).await;
+    fs::create_dir_all(path.parent().unwrap()).await?;
+    entry.to_file(&path).await
+}
+
+async fn load_ldap_entry(dn: &str) -> std::io::Result<LdapEntry> {
+    let path = dn_to_path(dn).await;
+    LdapEntry::from_file(&path).await
+}
+
+async fn dn_to_path(dn: &str) -> PathBuf {
+    let components: Vec<&str> = dn.split(',').collect();
+    let mut path = PathBuf::new();
+    for component in components.iter().rev() {
+        let key_value: Vec<&str> = component.split('=').collect();
+        let (key, value) = (key_value[0], key_value[1]);
+        path.push(format!("{}={}", key, value));
+    }
+    path.push("entry.bson");
+    path
 }
